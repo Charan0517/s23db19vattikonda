9e47dc938bf92130783a2f29bc4d6acafdfc133e
Charan0517 <sricharanv2001@gmail.com>
2023-04-12 12:19:00 -0500

    Add post method

--- controllers/teacher.js
+++ controllers/teacher.js
@@ -15,10 +15,26 @@ exports.teacher_list = async function(req, res) {
 exports.teacher_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Teacher detail: ' + req.params.id);
 };
-// Handle Teacheer create on POST.
-exports.teacher_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Teacher create POST');
-};
+// Handle Teacher create on POST.
+exports.teacher_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Teacher();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.teacher_name = req.body.teacher_name;
+    document.salary = req.body.salary;
+    document.subject = req.body.subject;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
 // Handle Teacher delete form on DELETE.
 exports.teacher_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Teacher delete DELETE ' + req.params.id);
