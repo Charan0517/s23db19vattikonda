9a95238e1983ed7325c6319e5baf96365069bc9d
Charan0517 <sricharanv2001@gmail.com>
2023-04-24 17:13:41 -0500

    Add protection to create and delete

--- models/teacher.js
+++ models/teacher.js
@@ -6,22 +6,27 @@ const mongoose = require("mongoose")
 
 
 const teacherSchema = mongoose.Schema({
-teacher_name: {
-    type: String,
-    minlength: 4,
-    maxlength: 20
-},
-salary: String,
-subject: String
+    teacher_name: {
+        type: String,
+        minlength: [3, 'Teacher name Should have minimum length of 4'],
+        maxlength: [20, 'Teacher name Should have maximum length of 20']
+    },
+    salary: String,
+    subject: {
+        type: String,
+        minlength: [3, 'Subject name Should have minimum length of 4'],
+        maxlength: [20, 'Subject name Should have maximum length of 20']
+    }
 })
 
 //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
+db.once("open", function () {
+    console.log("Connection to DB succeeded")
+});
 
-  
+
 module.exports = mongoose.model("Teacher", teacherSchema)
 
