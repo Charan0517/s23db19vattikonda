44fc012d8b7f887b8da9b229ccb6f570c9a8da98
Charan0517 <sricharanv2001@gmail.com>
2023-04-24 02:10:00 -0500

    Secured page

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,4 +1,13 @@
 var express = require('express');
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
 const teacher_controlers= require('../controllers/teacher');
 var router = express.Router();
 /* GET teachers */
@@ -12,7 +21,7 @@ router.get('/detail', teacher_controlers.teacher_view_one_Page);
 router.get('/create', teacher_controlers.teacher_create_Page);
 
 /* GET create update page */
-router.get('/update', teacher_controlers.teacher_update_Page);
+router.get('/update',secured, teacher_controlers.teacher_update_Page);
 
 /* GET delete teacher page */
 router.get('/delete', teacher_controlers.teacher_delete_Page);
